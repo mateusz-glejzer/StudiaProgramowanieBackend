1e8da657773c8281f479befc37542540c92df265
Mateusz <papierekm@gmail.com>
2022-03-20 14:47:02 +0100

    make zadanie2

--- zadanie 3/src/com/company/zadanie2.java
+++ zadanie 3/src/com/company/zadanie2.java
@@ -1,4 +1,23 @@
 package com.company;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class zadanie2 {
+
+        public static void main(String[] args) throws IOException {
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("type data to save");
+            String text = scanner.nextLine();
+            File file = new File("C:\\Users\\papie\\Desktop");
+            try (FileOutputStream fos = new FileOutputStream(file)) {
+
+
+                byte[] mybytes = text.getBytes();
+
+                fos.write(mybytes);
+            }
+        }
+
 }
